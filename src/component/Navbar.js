233946c02c5e405786090e6cc7011320c28d0f6d
pushpendra0809 <61344242+pushpendra0809@users.noreply.github.com>
2023-10-20 00:11:25 +0530

    navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,74 +7,111 @@ import logoImage3 from "./instagram.png";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark ">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/home">
-            <img src={logoImage}alt="" className="logoImage"/>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+          <Link class="navbar-brand" to="/home">
+            <img src={logoImage} alt="" className="logoImage" />
             Pushpendra's Portpolio
           </Link>
           <button
-            className="navbar-toggler"
+            class="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/home">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/project">
-                  Project
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/contact">
-                  Contact
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/skill">
-                  Skill
-                </Link>
-              </li>
-            </ul>
-            <span className="navbar-text">
-                {/* <div className="social-icon ">
-                    <a className="social-media mx-1" href="https://www.linkedin.com/in/pushpendra-pal-9628ab214/" target="_blank" rel="noopener noreferrer"> <img src={logoImage1} alt="" /></a>
-                    <Link className="social-media mx-1" to="https://github.com/pushpendra0809" target="_blank" rel="noopener noreferrer">< img src={logoImage2} alt=""/></Link>
-                    <Link className="social-media mx-1" to="https://www.instagram.com/pushpendra.pal.54772/" target="_blank" rel="noopener noreferrer">< img src={logoImage3} alt=""/></Link>
-                    <Link className="btn btn-primary mx-1" to="/contact" onClick={()=> console.log('connect')}><span>Let's Connect</span></Link>
-                </div> */}
+          <div
+            class="offcanvas offcanvas-end text-bg-dark"
+            tabindex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                Dark offcanvas
+              </h5>
+              <button
+                type="button"
+                class="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0 mr-auto">
+                <li class="nav-item">
+                  <Link class="nav-link active" aria-current="page" to="/home">
+                    Home
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link active" aria-current="page" to="/About">
+                    About
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link
+                    class="nav-link active"
+                    aria-current="page"
+                    to="/project"
+                  >
+                    Project
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link
+                    class="nav-link active"
+                    aria-current="page"
+                    to="/contact"
+                  >
+                    Contact
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link active" aria-current="page" to="/skill">
+                    Skill
+                  </Link>
+                </li>
+              </ul>
+              <span className="navbar-text">
                 <div className="social-icon">
-    <a className="social-media mx-1" href="https://www.linkedin.com/in/pushpendra-pal-9628ab214/" target="_blank" rel="noopener noreferrer">
-        <img src={logoImage1} alt="" />
-    </a>
-    <a className="social-media mx-1" href="https://github.com/pushpendra0809" target="_blank" rel="noopener noreferrer">
-        <img src={logoImage2} alt=""/>
-    </a>
-    <a className="social-media mx-1" href="https://www.instagram.com/pushpendra.pal.54772/" target="_blank" rel="noopener noreferrer">
-        <img src={logoImage3} alt=""/>
-    </a>
-    <Link className="btn btn-primary mx-1" to="/contact" onClick={() => console.log('connect')}>
-        <span>Let's Connect</span>
-    </Link>
-</div>
-
-            </span>
+                  <a
+                    className="social-media mx-1"
+                    href="https://www.linkedin.com/in/pushpendra-pal-9628ab214/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={logoImage1} alt="" />
+                  </a>
+                  <a
+                    className="social-media mx-1"
+                    href="https://github.com/pushpendra0809"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={logoImage2} alt="" />
+                  </a>
+                  <a
+                    className="social-media mx-1"
+                    href="https://www.instagram.com/pushpendra.pal.54772/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={logoImage3} alt="" />
+                  </a>
+                  <Link
+                    className="btn btn-primary mx-1"
+                    to="/contact"
+                    onClick={() => console.log("connect")}
+                  >
+                    <span>Let's Connect</span>
+                  </Link>
+                </div>
+              </span>
+            </div>
           </div>
         </div>
       </nav>
